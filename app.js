a20c3403bfb0d2778d40ffe5197e84bf01281f17
jludena7 <jludena7@hotmail.com>
2023-09-25 16:24:06 -0500

    check the app with eslint and fix style standar coding

--- app.js
+++ app.js
@@ -1,61 +1,62 @@
-const dbDriver = require('./app/helpers/DbDriver');
-const express = require('express');
-const { engine } = require('express-handlebars');
-const path = require('path');
-const router = require('./app/routers/backend');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const flash = require('connect-flash');
-const httpError = require('http-errors');
-const passport = require('./app/helpers/Passport');
-const sessionDriver = require('./app/helpers/SessionDriver');
-const csrf = require('csurf');
-require('dotenv').config({path: '.env'});
+const dbDriver = require('./app/helpers/DbDriver')
+const express = require('express')
+const { engine } = require('express-handlebars')
+const path = require('node:path')
+const router = require('./app/routers/backend')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const httpError = require('http-errors')
+const passport = require('./app/helpers/Passport')
+const sessionDriver = require('./app/helpers/SessionDriver')
+const csrf = require('csurf')
+const Handlebars = require('./app/helpers/Handlebars')
+require('dotenv').config({ path: '.env' })
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.engine('handlebars',
-    engine({
-        defaultLayout: 'layout',
-        helpers: require('./app/helpers/Handlebars')
-    })
-);
-app.set('view engine', 'handlebars');
-app.set('views', './app/views');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cookieParser());
+  engine({
+    defaultLayout: 'layout',
+    helpers: Handlebars
+  })
+)
+app.set('view engine', 'handlebars')
+app.set('views', './app/views')
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(cookieParser())
 
-app.use(csrf({cookie:true}));
+app.use(csrf({ cookie: true }))
 app.use((req, res, next) => {
-    res.locals._csrfToken = req.csrfToken();
-    next();
-});
+  res.locals._csrfToken = req.csrfToken()
+  next()
+})
 
-app.use(sessionDriver(dbDriver.mongoose));
+app.use(sessionDriver(dbDriver.mongoose))
 
-app.use(passport.initialize({}));
-app.use(passport.session({}));
+app.use(passport.initialize({}))
+app.use(passport.session({}))
 
-app.use(flash());
+app.use(flash())
 
 app.use((req, res, next) => {
-    res.locals.errorValidations = req.flash();
-    next();
-});
+  res.locals.errorValidations = req.flash()
+  next()
+})
 
-app.use('/', router());
+app.use('/', router())
 app.use((req, res, next) => {
-    next(httpError(404, 'Page not found'));
-});
+  next(httpError(404, 'Page not found'))
+})
 app.use((error, req, res, next) => {
-    res.locals.errorMessage = error.message;
-    const status = error.status || 500;
-    res.locals.errorStatus = status;
-    res.status(status);
-    return res.render('error');
-});
+  res.locals.errorMessage = error.message
+  const status = error.status || 500
+  res.locals.errorStatus = status
+  res.status(status)
+  return res.render('error')
+})
 
-module.exports = app;
+module.exports = app
